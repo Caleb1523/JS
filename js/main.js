acb97ae41e1e862cc6ab5709a31002d6cf78c808
Caleb1523 <sierracaleb15@gmail.com>
2025-10-14 13:33:46 -0500

    proyecto js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Función que muestra un mensaje al hacer clic en el botón
+function mostrarMensaje() {
+    alert("Hola, bienvenido al curso práctico de JavaScript");
+}
+
+// Asociar la función al botón
+document.getElementById("botonBienvenida").addEventListener("click", mostrarMensaje);
