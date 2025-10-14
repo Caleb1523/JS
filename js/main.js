b7ba89d4fec1bf52068e2a7075c1db138bf940ce
Caleb1523 <sierracaleb15@gmail.com>
2025-10-14 16:51:37 -0500

    arreglo js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // Función que muestra un mensaje al hacer clic en el botón
 function mostrarMensaje() {
-    alert("Hola, bienvenido al curso práctico de JavaScript");
+    // Redirige a otra página (puedes cambiar el nombre del archivo)
+    window.location.href = "./html/ejercicios.html";
 }
 
 // Asociar la función al botón
 document.getElementById("botonBienvenida").addEventListener("click", mostrarMensaje);
+
